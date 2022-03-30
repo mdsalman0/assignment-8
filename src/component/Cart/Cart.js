c7369e8c3dcba0b6dc2cc435767e9f518ae1f3f5
mdsalman0 <mdsalman01864346@gmail.com>
2022-03-31 02:02:03 +0600

    delete cart all

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -3,15 +3,24 @@ import './Cart.css'
 
 const Cart = (props) => {
     const {crat} = props;
-    const [random, setRandom] = useState(0)
+    const [random, setRandom] = useState([])
+
     let name ='';
     for(const product of crat){
         name = name + ' ' +product.name;
     }
+
     const randomNum = ()=>{
         const newNum = Math.floor(Math.random()* crat.length)
-        alert(crat[newNum].name)
+        alert(crat[newNum].name);
+        
     }
+     
+    const removeCartName = (crat)=>{
+       console.log(crat);
+      
+    }
+
     return (
         <div className='cart'>
             <h3>Selected Mobile</h3>
@@ -20,7 +29,7 @@ const Cart = (props) => {
             {/* <p> {random}</p> */}
             </div>
             <button onClick={randomNum} className='choose'>CHOOSE 1 FOr ME</button>
-            <button className='choose'>CHOOSE AGAIN</button>
+            <button onClick={()=>removeCartName(crat)} className='choose'>CHOOSE AGAIN</button>
         </div>
     );
 };
